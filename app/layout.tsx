3319554a3a9f83d14fd4373247eff788f217c0d7
nefigarcia <bluedivee@hotmail.com>
2026-05-26 00:29:01 -0600

    vercel analitics

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { ClerkProvider } from "@clerk/nextjs";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/next"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -41,6 +42,7 @@ export default function RootLayout({
       >
         <body className="min-h-screen bg-[#0a0a0f] text-white antialiased">
           {children}
+          <Analytics />
         </body>
       </html>
     </ClerkProvider>
